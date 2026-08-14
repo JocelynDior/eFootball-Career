a5367558a308f45ea3ba522a37f70d9d766448f1
Jocelyn P. Mahlatse <kmahlatse123@gmail.com>
2026-08-14 12:38:33 +0200

    Update sastTime.js

--- src/utils/sastTime.js
+++ src/utils/sastTime.js
@@ -1,1 +1,21 @@
+export function getSASTDateObj() {
+  const now = new Date();
+  const utc = now.getTime() + now.getTimezoneOffset() * 60000;
+  return new Date(utc + 2 * 3600000);
+}
 
+export function getSASTToday() {
+  return getSASTDateObj().toISOString().split('T')[0];
+}
+
+export function getSASTYesterday() {
+  const d = getSASTDateObj();
+  d.setDate(d.getDate() - 1);
+  return d.toISOString().split('T')[0];
+}
+
+export function getSASTMidnightTimestamp() {
+  const sast = getSASTDateObj();
+  const midnight = new Date(sast.getFullYear(), sast.getMonth(), sast.getDate(), 0, 0, 0, 0);
+  return midnight.getTime() - 2 * 3600000;
+}
