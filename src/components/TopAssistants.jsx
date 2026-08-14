0eb01e05d83a40e8b69574b314802083aca927bb
Jocelyn P. Mahlatse <kmahlatse123@gmail.com>
2026-08-14 12:26:46 +0200

    Add TopScorers component to TopAssistants

--- src/components/TopAssistants.jsx
+++ src/components/TopAssistants.jsx
@@ -1,1 +1,5 @@
+import TopScorers from "./TopScorers";
 
+export default function TopAssistants({ assistants, onAdd, onEdit, onDelete, teamIconsCache }) {
+  return <TopScorers scorers={assistants} type="assistant" onAdd={onAdd} onEdit={onEdit} onDelete={onDelete} teamIconsCache={teamIconsCache} />;
+}
