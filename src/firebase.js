40d293b5f39200de97b3979aaf43b3f905d50506
Jocelyn P. Mahlatse <kmahlatse123@gmail.com>
2026-08-15 22:00:08 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getDatabase, ref, set, get, push, update, remove } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDJCnltEBfrkBfiny7gXISkTLwajA7KztE",
@@ -13,3 +13,24 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
+
+export const PATHS = {
+  posts: "career_posts",
+  stories: "career_stories",
+  teamIcons: "career_team_icons",
+  managerKeys: "career_manager_keys",
+  playedOpponents: "career_played_opponents",
+  fixtures: "career_fixtures",
+  rankings: "career_rankings",
+  transfers: "career_transfers",
+  accounts: "career_accounts",
+  leagueSettings: (league) => `career_${league}_settings`,
+  table: (league, season) => `career_${league}/seasons/season_${season}/table`,
+  results: (league, season) => `career_${league}/seasons/season_${season}/results`,
+  pendingResults: (league, season) => `career_${league}/seasons/season_${season}/pending_results`,
+  topScorers: (league, season) => `career_${league}/seasons/season_${season}/top_scorers`,
+  topAssistants: (league, season) => `career_${league}/seasons/season_${season}/top_assistants`,
+  managerHistory: (league, season) => `career_${league}/seasons/season_${season}/manager_history`,
+};
+
+export { ref, set, get, push, update, remove };
