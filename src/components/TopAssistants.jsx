fb2793f7c6f22f150e6face2c20313ec85af6ce2
Jocelyn P. Mahlatse <kmahlatse123@gmail.com>
2026-08-26 01:50:29 +0200

    Update TopAssistants.jsx

--- src/components/TopAssistants.jsx
+++ src/components/TopAssistants.jsx
@@ -1,5 +1,14 @@
 import TopScorers from "./TopScorers";
 
-export default function TopAssistants({ assistants, onAdd, onEdit, onDelete, teamIconsCache }) {
-  return <TopScorers scorers={assistants} type="assistant" onAdd={onAdd} onEdit={onEdit} onDelete={onDelete} teamIconsCache={teamIconsCache} />;
+export default function TopAssistants({ league, season, onAdd, onEdit, onDelete }) {
+  return (
+    <TopScorers
+      league={league}
+      season={season}
+      type="assistant"
+      onAdd={onAdd}
+      onEdit={onEdit}
+      onDelete={onDelete}
+    />
+  );
 }
